5aa2f4fa452baac258033c4e6b881477e710da21
Mantas Norvaiša <mntnorv@gmail.com>
2017-04-12 16:17:24 +0300

    Add RSpec tests for RXGetText::POEntry#initialize

---
+++ spec/unit/po_entry_spec.rb
@@ -0,0 +1,111 @@
+require 'spec_helper'
+
+describe RXGetText::POEntry do
+  describe '#initialize' do
+    context 'creating a simple PO entry' do
+      let(:entry) { described_class.new(msgid: 'An error occurred!') }
+
+      it 'sets the passed msgid attribute' do
+        expect(entry.msgid).to eq('An error occurred!')
+      end
+
+      it 'sets msgid_plural to nil' do
+        expect(entry.msgid_plural).to be nil
+      end
+
+      it 'sets msgctxt to nil' do
+        expect(entry.msgctxt).to be nil
+      end
+
+      it 'sets references to an empty array' do
+        expect(entry.references).to eq([])
+      end
+    end
+
+    context 'creating a pluralized entry' do
+      let(:entry) do
+        described_class.new(
+          msgid: 'An error occurred!',
+          msgid_plural: '%d errors occurred!'
+        )
+      end
+
+      it 'sets the passed msgid attribute' do
+        expect(entry.msgid).to eq('An error occurred!')
+      end
+
+      it 'sets the passed msgid_plural attribute' do
+        expect(entry.msgid_plural).to eq('%d errors occurred!')
+      end
+    end
+
+    context 'creating an entry with a context' do
+      let(:entry) do
+        described_class.new(
+          msgid: 'An error occurred!',
+          msgctxt: 'Error modal'
+        )
+      end
+
+      it 'sets the passed msgid attribute' do
+        expect(entry.msgid).to eq('An error occurred!')
+      end
+
+      it 'sets the passed msgctxt attribute' do
+        expect(entry.msgctxt).to eq('Error modal')
+      end
+    end
+
+    context 'creating an entry with references' do
+      let(:entry) do
+        described_class.new(
+          msgid: 'An error occurred!',
+          references: ['errors.rb:15']
+        )
+      end
+
+      it 'sets the passed msgid attribute' do
+        expect(entry.msgid).to eq('An error occurred!')
+      end
+
+      it 'sets the passed msgctxt attribute' do
+        expect(entry.references).to eq(['errors.rb:15'])
+      end
+    end
+
+    context 'creating an entry with a context in msgid' do
+      context 'context is separated by the default separator' do
+        let(:entry) do
+          described_class.new(
+            msgid: 'Error modal|An error occurred!'
+          )
+        end
+
+        it 'sets the correct separated msgid' do
+          expect(entry.msgid).to eq('An error occurred!')
+        end
+
+        it 'sets the passed msgctxt attribute' do
+          expect(entry.msgctxt).to eq('Error modal')
+        end
+      end
+
+      context 'context is separated by a custom separator' do
+        let(:entry) do
+          described_class.new(
+            msgid: 'Error modal;;An error occurred!',
+            separator: ';;'
+          )
+        end
+
+        it 'sets the correct separated msgid' do
+          expect(entry.msgid).to eq('An error occurred!')
+        end
+
+        it 'sets the passed msgctxt attribute' do
+          expect(entry.msgctxt).to eq('Error modal')
+        end
+      end
+    end
+  end
+end
